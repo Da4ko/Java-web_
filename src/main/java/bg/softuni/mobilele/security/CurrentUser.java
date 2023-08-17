b4a9930c5fbd1aa6f38e675fc1d81e63032da70c
Da4ko <da4ko06@gmail.com>
2023-08-17 11:00:12 +0300

    new Update

--- src/main/java/bg/softuni/mobilele/security/CurrentUser.java
+++ src/main/java/bg/softuni/mobilele/security/CurrentUser.java
@@ -9,7 +9,7 @@ public class CurrentUser {
     private static final String ANONYMOUS = "anonymous";
 
     private String name = ANONYMOUS;
-    private boolean isAnonymous;
+    private boolean isAnonymous = true;
 
 
 
@@ -23,6 +23,10 @@ public class CurrentUser {
 
     public boolean isAnonymous() {
         return isAnonymous;
+    }
+
+    public boolean isLoggedIn(){
+        return !isAnonymous;
     }
 
     public void setAnonymous(boolean anonymous) {
