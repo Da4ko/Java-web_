87e6ead530419b2b460516c45427da1540205ada
Da4ko <da4ko06@gmail.com>
2023-08-20 12:06:36 +0300

    new Update

--- src/main/java/bg/softuni/mobilele/model/service/UserLoginServiceModel.java
+++ src/main/java/bg/softuni/mobilele/model/service/UserLoginServiceModel.java
@@ -1,8 +1,18 @@
 package bg.softuni.mobilele.model.service;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class UserLoginServiceModel {
+
+    @NotNull
+    @Size(min = 2)
     private String username;
+
+    @NotNull
+    @Size(min = 3)
     private String password;
+
 
     public String getUsername() {
         return username;
